a1831c81bb1caaebee7328d07e849f95582cec77
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2020-06-18 19:13:50 +0530

    🎉 added some of the styling and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, IconComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
